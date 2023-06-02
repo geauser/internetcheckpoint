fa634e8c06bf6c01ca9f62d13fe0ed4456daa088
geauser <geauser@protonmail.com>
2023-06-02 14:41:41 +0200

    feat: add role="img" where needed

--- packages/website/components/Comment.vue
+++ packages/website/components/Comment.vue
@@ -47,6 +47,7 @@ function toggleReplies() {
   <div class="flex flex-row space-x-4 items-start">
 
     <div
+      role="img"
       class=" bg-center bg-contain rounded-full flex-shrink-0"
       :class="[isReply ? 'w-6 h-6' : 'w-10 h-10']"
       :style="{ backgroundImage: `url(${comment.importedAuthorPhoto})`}">
