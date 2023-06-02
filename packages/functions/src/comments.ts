f86ea7e59448623c9d12d210ab528a998d1b29a2
geauser <geauser@protonmail.com>
2023-06-02 13:42:25 +0200

    feat: add sorting for comments + return total comments count

--- packages/functions/src/comments.ts
+++ packages/functions/src/comments.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { db } from "database";
+import { sql } from "kysely";
 
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
@@ -9,10 +10,16 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   const isLoadingReplies = !!repliesOf;
   const batchSize = isLoadingReplies ? 10 : 100;
 
+  const { rows } = await sql<{ total: number }>`SELECT count(*) as total FROM comment`.execute(db);
+  const total = rows[0].total;
+
   const comments = await db
     .selectFrom('comment')
     .$if(isLoadingReplies, q => q.where('id', 'like', `${repliesOf}.%`))
+    .$if(!isLoadingReplies, q => q.where('id', 'not like', `%.%`))
     .selectAll()
+    .orderBy('votes', 'desc')
+    .orderBy('repliesCount')
     .offset(batchIndex * batchSize)
     .limit(batchSize)
     .execute();
@@ -21,6 +28,6 @@ export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ comments }),
+    body: JSON.stringify({ total, comments }),
   };
 };
