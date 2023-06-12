20604e9115e6aa1b6a6ec36dd567e4a4069268af
geauser <geauser@protonmail.com>
2023-06-12 10:06:11 +0200

    fix: fix redirect issue

--- packages/website/pages/index.vue
+++ packages/website/pages/index.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+
+const defaultVideoId = 'Q9XTqQbuavI';
+
+// TODO There must be a better way to do this...
+if (process.client) {
+  history.replaceState(null, '', `/${defaultVideoId}`);
+}
+</script>
 <template>
-  <Page video-id="zK4TWXWEKAQ" />
+  <Page :video-id="$route.params.id ?? defaultVideoId" />
 </template>
