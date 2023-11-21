a2ec93c1ff6167a208abe6d387d346d2e1f4ff80
geauser <geauser@protonmail.com>
2023-11-21 08:47:12 +0100

    chore: remove devtool

--- packages/website/nuxt.config.ts
+++ packages/website/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: true,
+
   vue: {
     compilerOptions: {
       isCustomElement: tag => tag.includes('media-'),
